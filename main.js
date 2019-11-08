c203af9e781c08eb0f304864656c0fd2a00176c9
B.Maher <bouzid.meher@gmail.com>
2019-11-08 13:05:06 +0100

    almost done

--- main.js
+++ main.js
@@ -9,21 +9,49 @@ function Cards(pairNumber, link){
 	return card 
 }
 
-var card1 = Cards(1, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWYxfejANdEmDV7ROaOZUE3teTin427_yDBBpvmNlOkNVTStOl&s");
-var card2 = Cards(1, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWYxfejANdEmDV7ROaOZUE3teTin427_yDBBpvmNlOkNVTStOl&s");
-var card3 = Cards(2, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1K5nV-izh4hyxw_4StZHQGc98Vj8sqndmAB1xhPY6uWKFTtuQ&s");
-var card4 = Cards(2, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1K5nV-izh4hyxw_4StZHQGc98Vj8sqndmAB1xhPY6uWKFTtuQ&s");
-var card5 = Cards(3, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl8WG3I-jhQYx9fXXQtI-Tsu4jHS3hSBCowRL0W-LA8yqZ-08D&s");
-var card6 = Cards(3, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl8WG3I-jhQYx9fXXQtI-Tsu4jHS3hSBCowRL0W-LA8yqZ-08D&s");
-var card7 = Cards(4, "https://colourlex.com/wp-content/uploads/2017/03/Chrome-red-painted-swatch-203-225-opt.jpg");
-var card8 = Cards(4, "https://colourlex.com/wp-content/uploads/2017/03/Chrome-red-painted-swatch-203-225-opt.jpg");
-var card9 = Cards(5, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA4ico829TGwhI9PhEx2kovaWRy-enUlffi36_j6cXCJkvn4VY&s");
-var card10 = Cards(5, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA4ico829TGwhI9PhEx2kovaWRy-enUlffi36_j6cXCJkvn4VY&s");
-var card11 = Cards(6, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7a5DZafV85AWk5f28OC0zitj98YxqWZxL7HeEmLsmxZ1ULJQp&s");
-var card12 = Cards(6, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7a5DZafV85AWk5f28OC0zitj98YxqWZxL7HeEmLsmxZ1ULJQp&s");
-var blackcard = Cards(0, "https://art.pixilart.com/92b651c9a375d18.png");
+var card1 = Cards(1, "./images/black-beard.jpg");
+var card2 = Cards(1, "./images/black-beard.jpg");
+var card3 = Cards(2, "./images/law.jpg");
+var card4 = Cards(2, "./images/law.jpg");
+var card5 = Cards(3, "./images/goku.jpg");
+var card6 = Cards(3, "./images/goku.jpg");
+var card7 = Cards(4, "./images/pokemon.jpg");
+var card8 = Cards(4, "./images/pokemon.jpg");
+var card9 = Cards(5, "./images/one-piece.jpg");
+var card10 = Cards(5, "./images/one-piece.jpg");
+var card11 = Cards(6, "./images/uzumaki.jpg");
+var card12 = Cards(6, "./images/uzumaki.jpg");
+var card13 = Cards(7, "./images/mugiwara.jpg");
+var card14 = Cards(7, "./images/mugiwara.jpg");
+var card15 = Cards(8, "./images/your-lie.jpg");
+var card16 = Cards(8, "./images/your-lie.jpg");
+var card17 = Cards(9, "./images/akatsuki.jpg");
+var card18 = Cards(9, "./images/akatsuki.jpg");
+var card19 = Cards(10, "./images/all-mighty.jpg");
+var card20 = Cards(10, "./images/all-mighty.jpg");
+var card21 = Cards(11, "./images/kurama.jpg");
+var card22 = Cards(11, "./images/kurama.jpg");
+var card23 = Cards(12, "./images/kubi.jpg");
+var card24 = Cards(12, "./images/kubi.jpg");
+var card25 = Cards(13, "./images/meliodas.jpg");
+var card26 = Cards(13, "./images/meliodas.jpg");
+var card27 = Cards(14, "./images/pok.jpg");
+var card28 = Cards(14, "./images/pok.jpg");
+var card29 = Cards(15, "./images/L.jpg");
+var card30 = Cards(15, "./images/L.jpg");
+var card31 = Cards(16, "./images/mytwo.jpg");
+var card32 = Cards(16, "./images/mytwo.jpg");
+var card33 = Cards(17, "./images/fairy-tail.jpg");
+var card34 = Cards(17, "./images/fairy-tail.jpg");
+var card35 = Cards(18, "./images/one-punch.jpg");
+var card36 = Cards(18, "./images/one-punch.jpg");
+var card37 = Cards(18, "./images/one-punch.jpg");
+var card38 = Cards(18, "./images/one-punch.jpg");
+var card39 = Cards(18, "./images/one-punch.jpg");
+var card40 = Cards(18, "./images/one-punch.jpg");
+var blackcard = Cards(0, "./images/back.jpg");
 
-var deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12];
+var deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40];
 
 //function that generate a random numbr between 0 and the length of the initial array;
 function randomNumberGenerator(array){
@@ -43,6 +71,16 @@ function shuffle(array){
 function hideAll(){
 	$('.hiding').hide()
 }
+function hideBlack(){
+	$('.card0').hide()
+}
+function showAll(){
+	$('.hiding').show()
+	setTimeout(function(){
+		hideAll();
+		$('.card0').show()
+	},5000)
+}
 
 function displayCards(array){
 	var i = 0;
@@ -52,10 +90,22 @@ function displayCards(array){
 		i++;
 	}
 	hideAll()
+	hideBlack()
 }
 
 var shuffledDeck = shuffle(deck);
-displayCards(shuffledDeck);
+function start(){
+	$('#section1').hide()
+	$('#section2').show()
+	$('.divs').html('')
+	shuffledDeck = shuffle(deck);
+	displayCards(shuffledDeck);
+	$('a').on('click', theGame())
+}
+$('#start').on('click', function(){
+	start()
+	showAll()
+});
 
 function reverseCard(n){
 	$('.card0.card' + n).hide()
@@ -103,3 +153,7 @@ function theGame(){
 }
 
 $('a').on('click', theGame())
+$('document').ready(function(){
+	$('#section2').hide()
+	$('#section3').hide()
+})
