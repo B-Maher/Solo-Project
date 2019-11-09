e5fec4b7ed5addead2b843d2928dbab265479639
B.Maher <bouzid.meher@gmail.com>
2019-11-09 02:58:44 +0100

    almost done

--- main.js
+++ main.js
@@ -45,15 +45,24 @@ var card37 = Cards(19, "./images/berus.jpg");
 var card38 = Cards(19, "./images/berus.jpg");
 var card39 = Cards(20, "./images/konoha.jpg");
 var card40 = Cards(20, "./images/konoha.jpg");
-var blackcard = Cards(0, "./images/back.jpg");
+var blackcard = Cards(0, "./images/dragon.jpg");
 
-var deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40];
+var deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32];
+var shuffledDeck = shuffle(deck);
+var index1 ;
+var index2 ;
+var count = true;
+var numberOfTry = 0;
+var isComplete = 0;
+var startTime = 0;
+var timer = 0;
 
 //function that generate a random numbr between 0 and the length of the initial array;
 function randomNumberGenerator(array){
 	return Math.floor(Math.random() * array.length)	
 }
 
+//function that shuffle the cards
 function shuffle(array){
 	var newDeck = [];
 	var i = 0;
@@ -64,110 +73,141 @@ function shuffle(array){
 	return newDeck
 }
 
-function hideAll(){
-	$('.hiding').hide()
-}
-function hideBlack(){
-	$('.card0').hide()
-}
-function showAll(){
-	$('.hiding').show()
-	setTimeout(function(){
-		hideAll();
-		$('.card0').show()
-	},5000)
-}
-
+//function that display the cards in the page
 function displayCards(array){
+	$('.cads').html('');
 	var i = 0;
 	while(i < array.length){
-		$('#' + (i + 1)).append('<a  class="card0 card' + (i + 1) + '"><img src=' + blackcard.link + '></a>');
-		$('#' + (i + 1)).append('<a  class="hiding card' + (i + 1) + '"><img src=' + array[i].link + '></a>');
+		$('#' + (i + 1)).append('<div class="front card0 card' + (i + 1) + '"><img src=' + blackcard.link + '></div>');
+		$('#' + (i + 1)).append('<div class="back hiding card' + (i + 1) + '"><img src=' + array[i].link + '></div>');
 		i++;
 	}
-	hideAll()
-	hideBlack()
 }
 
-var shuffledDeck = shuffle(deck);
+//function that hides the matching pairs
+function hidePairs(n, m){
+	$('#' + n).hide();
+	$('#' + m).hide();
+}
+
+//function that will check the number of time the user click
+function theGame(){
+$('.cads').on('click', function(){
+	var id = this.getAttribute('id');//we get the id of the image clicked
+	$('#' + id).toggleClass("isFlipped")// here we revele the card clicked
+	numberOfTry++;
+	//we enter this condition only if no card is face Up
+	if(count){
+		index1 = id;
+		count = false;
+		return 0;
+		console.log('a')
+	}else{
+		index2 = id;
+		count = true;
+		$('.cads').off()//here we disable all click events because two cards are face Up
+		//we check if the two cards are the same
+		if(shuffledDeck[parseInt(index1) - 1].pairNumber === shuffledDeck[parseInt(index2) - 1].pairNumber){
+			setTimeout(function(){
+				//we hide the 2 cards
+				hidePairs(index1, index2);
+				isComplete++;
+				if(isComplete === (deck.length / 2)){
+					console.log(timer)
+					$('.time').html(timer)
+					$('#section2').hide();
+					$('main').append('<div class="finalMsg"><p class="final">Good job it only took you ' + timer + ' to complete the Game!</p></div>')
+					console.log('Good Job! it only took you ' + numberOfTry + ' tries');
+					$('#playAgain').on('click', function(){
+					$('.cads').html('').show();
+					$('.cads').removeClass('isFlipped');	
+					setTimeout(function(){
+					start();
+					}, 500)
+					});
+				}
+				theGame();
+			}, 500)	
+		}else{
+		setTimeout(function(){
+			$('#' + index1).removeClass('isFlipped')
+			$('#' + index2).removeClass('isFlipped')
+			theGame();
+		}, 2000)
+		}
+	}
+});
+}
+$('.cads').on('click', theGame());
+
 function start(){
-	$('#section1').hide()
-	$('#section2').show()
-	$('#section3').show()
-	$('.divs').html('')
+	$('#section1').hide();
+	$('#section2').show();
+	$('#section3').show();
+	$('.cads').addClass('isFlipped');	
 	shuffledDeck = shuffle(deck);
 	displayCards(shuffledDeck);
 	numberOfTry = 0;
 	isComplete = 0;
-	$('a').on('click', theGame())
+	count = true;
+	setTimeout(function(){
+	$('.cads').removeClass('isFlipped');
+	var start = new Date();
+	startTime = start.toLocaleTimeString('fr-FR', { hour: "numeric", minute: "numeric", second: "numeric" })
+	timer = setInterval(displayTime, 1000);	
+	},5000);
+	
 }
-
-
-function reverseCard(n){
-	$('.card0.card' + n).hide()
-	$('.hiding.card' + n).show()
-}
-function hideCards(n, m){
-	$('.hiding.card' + n).hide()
-	$('.hiding.card' + m).hide()
-	$('.card0.card' + n).show()
-	$('.card0.card' + m).show()
-}
-function hidePairs(n, m){
-	$('.hiding.card' + n).fadeOut( "slow" )
-	$('.hiding.card' + m).fadeOut( "slow" )
-}
-var index1 ;
-var index2 ;
-var count = true;
-var numberOfTry = 0;
-var isComplete = 0;
-
-function theGame(){
-	$('a').on('click', function(){
-		numberOfTry++;
-	if(count){
-		index1 = this.getAttribute('class').slice(10);
-		count = false;
-		reverseCard(index1)
-	}else{
-		index2 = this.getAttribute('class').slice(10);
-		count = true;
-		reverseCard(index2)
-		$('a').off()
-		if(shuffledDeck[parseInt(index1) - 1].pairNumber === shuffledDeck[parseInt(index2) - 1].pairNumber){
-			setTimeout(function(){
-				hidePairs(index1, index2);
-				isComplete++;
-				if(isComplete === (deck.length / 2)){
-					alert('Good Job! it only took you ' + numberOfTry + 'tries')
-				}
-				theGame();
-			}, 1000)	
-		}else{
-		setTimeout(function(){
-			hideCards(index1, index2)
-			theGame();
-		}, 2000)
-		}
-
-	}
-})
-}
-
-$('a').on('click', theGame());
-
 $('#playAgain').on('click', function(){
-	start()
-	showAll()
+	$('.cads').html('').show();
+	$('.finalMsg').remove();
+	$('.cads').removeClass('isFlipped');	
+	setTimeout(function(){
+	start();
+	}, 500)
+
 });
 
 $('#start').on('click', function(){
-	start()
-	showAll()
+	start();
 });
 
 $('document').ready(function(){
-	$('#section2').hide()
-	$('#section3').hide()
+	$('#section2').hide();
+	$('#section3').hide();
 });
+
+function convertTime(end, start) {
+    
+    var str = "";
+    var resSec = parseInt(end.slice(6)) - parseInt(start.slice(6));
+    var resMin = parseInt(end.slice(3, 5)) - parseInt(start.slice(3, 5));
+    var resHr = parseInt(end.slice(0, 2)) - parseInt(start.slice(0, 2));
+
+    if (resMin < 0) {
+        resMin += 60;
+        resHr--;
+    }
+
+    if (resSec < 0) {
+        resSec += 60;
+        resMin--;
+    }
+
+    if (resHr !== 0) {
+        str += resHr + "hr "
+    }
+    if (resMin !== 0) {
+        str += resMin + "min "
+    }
+    str += resSec + "s."
+
+    return str
+}
+
+function displayTime() {
+        var end = new Date();
+        endTime = end.toLocaleTimeString('fr-FR', { hour: "numeric", minute: "numeric", second: "numeric" });
+        timer = convertTime(endTime, startTime);
+        $('.time').html(convertTime(endTime, startTime));
+}
